24266eb6aece8935b6d89ef38d04b47d8fb48f5a
Cuasar2123 <131405871+Cuasar2123@users.noreply.github.com>
2023-04-20 17:43:10 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+int resultado = suma(2, 4, 6);
+System.out.println(resultado);
+    }
+    public static int suma(int a, int b, int c) {
+        return a + b + c;
+    }
+
+}
