f9d1dfed00fe76e77ca1cc5c95f9c8a6360518a7
Cuasar2123 <131405871+Cuasar2123@users.noreply.github.com>
2023-04-20 17:51:15 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,10 +1,20 @@
 public class Main {
-    public static void main(String[] args) {
-int resultado = suma(2, 4, 6);
-System.out.println(resultado);
+    public static void main(String[] args){
+
+Coche micoche = new Coche();
+micoche.AgregarPuerta();
+        micoche.AgregarPuerta();
+
+System.out.println(micoche.puertas);
+
     }
-    public static int suma(int a, int b, int c) {
-        return a + b + c;
+    static class Coche {
+        public int puertas = 2;
+
+        public void AgregarPuerta() {
+            this.puertas++;
+        }
+
     }
 
 }
